75bc299b2e2d7ecc79cc176b16fde0b4a7d0ff0d
a.artamonov <a.artamonov@gb.ru>
2022-11-09 12:44:43 +0300

    First task is done

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+void PrintInConsole(double res)
+{
+    Console.WriteLine($"Result of your operation is '{res}'");
+}
+
+double DegreeOfNumber(double a, int b)
+{
+    double result = 1;
+    for(int i = 1; i <= b; i++)
+    {
+        result *= a;
+    }
+    return result;
+}
+
+Console.Write("Input a number: ");
+double number = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input a natural degree: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+PrintInConsole(DegreeOfNumber(number, degree));
