99aad890a3be356b65c80131fa0259f79c5a5db4
a.artamonov <a.artamonov@gb.ru>
2022-11-09 13:05:44 +0300

    Second task is done

--- Program.cs
+++ Program.cs
@@ -1,24 +1,51 @@
 ﻿// Задача 25: Напишите программу, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B
 // В задаче не указано, какой тип у числа А, поэтому использовал double
 
-void PrintInConsole(double res)
+// void PrintInConsole(double res)
+// {
+//     Console.WriteLine($"Result of your operation is '{res}'");
+// }
+
+// double DegreeOfNumber(double a, int b)
+// {
+//     double result = 1;
+//     for(int i = 1; i <= b; i++)
+//     {
+//         result *= a;
+//     }
+//     return result;
+// }
+
+// Console.Write("Input a number: ");
+// double number = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Input a natural degree: ");
+// int degree = Convert.ToInt32(Console.ReadLine());
+
+// PrintInConsole(DegreeOfNumber(number, degree));
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void PrintInConsole(int res)
 {
-    Console.WriteLine($"Result of your operation is '{res}'");
+    Console.WriteLine($"Sum digits of your number is '{res}'");
 }
 
-double DegreeOfNumber(double a, int b)
+int SumDigitsOfNumber(int number)
 {
-    double result = 1;
-    for(int i = 1; i <= b; i++)
+    int sum = 0;
+    if(number < 0) number *= -1;
+    while(number > 0)
     {
-        result *= a;
+        sum += number%10;
+        number /= 10;
     }
-    return result;
+    return sum;
 }
 
-Console.Write("Input a number: ");
-double number = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Input a natural degree: ");
-int degree = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input an intreger number: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-PrintInConsole(DegreeOfNumber(number, degree));
+PrintInConsole(SumDigitsOfNumber(number));
+
+// Задача 29: Напишите программу, которая создает массив заданного пользователем размера, заполняет массив элементами от 1 до 99 и выводит их на экран.
